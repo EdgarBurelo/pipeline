29fb88a2f54cac29b3b906a5bfff9516e645941e
Edgar Burelo <edgar.burelo@gmail.com>
2019-01-16 16:58:49 -0600

    sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Button from "./components/button";
 import Nav from "./components/nav"
 import About from "./components/About";
+import Sidebarn from "./components/Sidebar";
+import { Container } from "semantic-ui-react";
+import ".";
+import "./style.css";
 
  class App extends Component {
   render() {  
   return (
       <Router>
-        <div >
-          <Nav />
-          <Switch >
-            <Route path="/" exact component={Button} />
-            <Route path="/about" component={About} />
-            
-          </Switch>
-          
-        </div>
+        <Container fluid={true} style={{padding:"0px"}}>
+          <Sidebarn />
+          <div style={{marginLeft:"150px"}}>
+            <Nav />
+            <Switch >
+              <Route path="/" exact component={Button} />
+              <Route path="/about" component={About} />
+            </Switch>
+          </div>
+        </Container>
       </Router>
     );
   }
