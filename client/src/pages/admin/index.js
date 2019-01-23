9f318ea166c7dabb227ac19b22b03f0c1a71777c
Raghava Lakshminarayana <raglaks94@gmail.com>
2019-01-22 21:30:01 -0600

    rerender table component successfully

--- client/src/pages/admin/index.js
+++ client/src/pages/admin/index.js
@@ -1,192 +1,169 @@
 import React, { Component } from "react";
-import { Button, Form, Dropdown, Table, Icon } from 'semantic-ui-react';
+import { Button, Form, Dropdown, Table } from "semantic-ui-react";
 import API from "../../utils/API";
 
 const userTypes = [
-  
-  {text: "Agent", value: "Agent"},
-  {text: "Supervisor", value: "Supervisor"}
-  
-]; 
+  { text: "Agent", value: "Agent" },
+  { text: "Supervisor", value: "Supervisor" }
+];
 
 let newObj = {};
 
 class Admin extends Component {
-
   constructor(props) {
-
     super(props);
 
     this.state = {
-
       users: []
-
     };
-
   }
 
   componentDidMount() {
-
     this.stateUsers();
-
   }
 
   stateUsers = () => {
 
-    API.allUsers().then((data) => {
+    API.allUsers()
+      .then(data => {
+        let stateArr = data.data;
 
-      let stateArr = data.data;
+        this.setState(prevState => {
+          prevState.users = stateArr;
 
-      this.setState(prevState => {
+          return prevState;
+        });
 
-        prevState.users = stateArr;
-
-        return prevState;
-      
+      })
+      .catch(err => {
+        console.log(err);
       });
-
-      this.renderUsers();
-
-    }).catch(err => {
-
-      console.log(err);
-
-    });
-
-  }
-
-  renderUsers = () => {
-
-    // console.log(this.state);
-
-    let arrRows = this.state.users.map(rows => {
-
-      console.log(rows);
-
-      return arrRows;
-
-    });
-
-  }
+  };
 
   buttonClick = event => {
-
+    
     event.preventDefault();
-  
+
     console.log(newObj);
 
-    API.newUser(newObj.name, newObj.email, newObj.type);
-  
-  }
+    //new user created
+    API.newUser(newObj.name, newObj.email, newObj.type).then(()=>{
+
+      this.stateUsers();
+
+    });
+
+  };
 
   nameChange = event => {
-
     event.preventDefault();
 
     let iName = event.target.value;
 
-    console.log(iName);
-
     newObj.name = iName;
-
-  }
+  };
 
   emailChange = event => {
-
     event.preventDefault();
 
     let iEmail = event.target.value;
 
-    console.log(iEmail);
-
     newObj.email = iEmail;
-
-  }
+  };
 
   typeChange = event => {
-
     event.preventDefault();
 
     let iType = event.target.children[0].innerText;
 
-    console.log(iType);
-
     newObj.type = iType;
-
-  }
+  };
 
   render() {
 
-    return(
+    let arrRows = this.state.users.map(rows => {
 
+      return (
+
+        <Table.Row key={rows.id}>
+
+          <Table.Cell>
+
+            {rows.name}
+
+          </Table.Cell>
+
+          <Table.Cell>
+
+            {rows.email}
+
+          </Table.Cell>
+
+          <Table.Cell>
+
+            {rows.profile}
+
+          </Table.Cell>
+
+        </Table.Row>
+
+      )
+      
+    });
+
+    return (
       <div>
+        <Form>
+          <Form.Field>
+            <label>Full Name</label>
+            <input placeholder="Full Name" onChange={this.nameChange} />
+          </Form.Field>
 
-      <Form>
-        <Form.Field>
-          <label>Full Name</label>
-          <input placeholder='Full Name' onChange={this.nameChange}/>
-        </Form.Field>
+          <Form.Field>
+            <label>Email Address</label>
+            <input placeholder="Email Address" onChange={this.emailChange} />
+          </Form.Field>
 
-        <Form.Field>
-          <label>Email Address</label>
-          <input placeholder='Email Address' onChange={this.emailChange}/>
-        </Form.Field>
+          <Form.Field>
+            <label>User Type</label>
+            <Dropdown
+              placeholder="Select User Type"
+              selection
+              options={userTypes}
+              onChange={this.typeChange}
+            />
+          </Form.Field>
+          <Button type="submit" onClick={this.buttonClick}>
+            Submit
+          </Button>
+        </Form>
 
-        <Form.Field>
-          <label>User Type</label>
-          <Dropdown placeholder='Select User Type' selection options={userTypes} onChange={this.typeChange}/>
-        </Form.Field>
-          <Button type='submit' onClick={this.buttonClick}>Submit</Button>
-      </Form>
-
-      <Table celled striped>
+        <Table celled striped>
           <Table.Header>
-
             <Table.Row>
-              <Table.HeaderCell colSpan='3'>Users</Table.HeaderCell>
-            </Table.Row>
-      
-            <Table.Row>
-      
-              <Table.HeaderCell>
-                Name
-              </Table.HeaderCell>
-        
-              <Table.HeaderCell>
-                Email
-              </Table.HeaderCell>
-        
-              <Table.HeaderCell>
-                User Type
-              </Table.HeaderCell>
-        
+              <Table.HeaderCell colSpan="3">Users</Table.HeaderCell>
             </Table.Row>
 
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+
+              <Table.HeaderCell>Email</Table.HeaderCell>
+
+              <Table.HeaderCell>User Type</Table.HeaderCell>
+            </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            <Table.Row>
-              <Table.Cell collapsing>
-                <Icon name='folder' /> node_modules
-              </Table.Cell>
 
-              <Table.Cell>Initial commit</Table.Cell>
-
-              <Table.Cell collapsing textAlign='right'>
-                10 hours ago
-              </Table.Cell>
-            </Table.Row>
+            {arrRows}
 
           </Table.Body>
 
-      </Table>
-
+          
+        </Table>
       </div>
-
-    )
-
+    );
   }
-
 }
 
-export default Admin
+export default Admin;
